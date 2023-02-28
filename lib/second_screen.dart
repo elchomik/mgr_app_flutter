3f85e41c65edf9e99c4ccac081c6f6dfd5321f26
elChomik <mateuszmarkowski101@wp.pl>
2023-02-28 20:01:13 +0100

    Finished quickSort algorithm

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -33,7 +33,24 @@ class _SecondScreenState extends State<SecondScreen> {
   }
 
   int partition(List<String> list, int leftIndex, int rightIndex) {
-    int pivot = list[rightIndex];
+    String pivot = list[rightIndex];
+    int i = leftIndex -1;
+    for(int j=leftIndex; j<rightIndex; j++){
+      if(list[j].compareTo(pivot) < 0){
+        i++;
+        swap(list,i,j);
+      }
+    }
+
+    swap(list, i+1, rightIndex);
+
+    return i+1;
+  }
+
+  void swap(List<String> list, int i, int j) {
+    String temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
   }
 
   @override
