cd5362cd02c87c6df9f03138b489fd8e13e2e7d8
elChomik <mateuszmarkowski101@wp.pl>
2023-02-28 19:55:06 +0100

    Updated title_widget

--- lib/widgets/title_widget.dart
+++ lib/widgets/title_widget.dart
@@ -3,24 +3,54 @@ import 'package:flutter/material.dart';
 class AppTitle extends StatelessWidget with PreferredSizeWidget {
   final String title;
   final double size;
+  bool isSecondScreen=false;
 
-  const AppTitle({Key? key, required this.title, required this.size}) : super(key: key);
+  AppTitle(
+      {Key? key,
+      required this.title,
+      required this.size,
+      isSecondScreen})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Text(
-          title,
-          style: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 40,
-          ),
-        ),
-      ),
-    );
+    return isSecondScreen
+        ? AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            ),
+          )
+        : AppBar(
+            title: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 
   @override
