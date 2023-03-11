9e8cb8aa739abed473ccf550ebbdb01555d155b9
elChomik <mateuszmarkowski101@wp.pl>
2023-03-11 22:01:10 +0100

    Comment unused and unnecessary widget

--- lib/widgets/title_widget.dart
+++ lib/widgets/title_widget.dart
@@ -1,52 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppTitle extends StatelessWidget with PreferredSizeWidget {
   final String title;
   final double size;
-  bool isSecondScreen=false;
+  bool isSecondScreen = false;
 
-  AppTitle(
-      {Key? key,
-      required this.title,
-      required this.size,
-      isSecondScreen})
+  AppTitle({Key? key, required this.title, required this.size, isSecondScreen})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    print (isSecondScreen);
     return isSecondScreen
         ? AppBar(
+            toolbarHeight: 120,
+            backgroundColor: const Color.fromARGB(255, 50, 75, 75),
             leading: IconButton(
               icon: const Icon(Icons.arrow_back),
               onPressed: () => Navigator.pop(context),
             ),
             title: Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding:  const EdgeInsets.only(left: 50, top:10),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Text(
                   title,
                   style: const TextStyle(
-                    color: Colors.black,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontSize: 40,
+                    fontSize: 24,
                   ),
                 ),
               ),
             ),
           )
         : AppBar(
-          backgroundColor: Colors.blue[850],
+            toolbarHeight: 120,
+            backgroundColor: const Color.fromARGB(255, 50, 75, 75),
             title: Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(left: 100, top:10),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Text(
                   title,
                   style: const TextStyle(
-                    color: Colors.black,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontSize: 40,
+                    fontSize: 24,
                   ),
                 ),
               ),
